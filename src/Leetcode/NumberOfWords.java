2c803cff0eb989e2ba04380d88974e1dc0bffbcd
Ideolion <skegul24@gmail.com>
2021-12-21 15:46:36 +0300

    Number of Valid Words in a Sentence
    add Number of Valid Words in a Sentence

--- src/Leetcode/NumberOfWords.java
+++ src/Leetcode/NumberOfWords.java
@@ -4,10 +4,28 @@
  */
 package Leetcode;
 
+import java.util.regex.Pattern;
+
 /**
  *
  * @author kiry
  */
 public class NumberOfWords {
-    
+
+    public int countValidWords(String sentence) {
+
+        int count = 0;
+        Pattern pat = Pattern.compile("([a-z]*([a-z]+[-]{0,1}[a-z]+){0,1}[.!,\\s]{0,1})");
+        String[] lists = sentence.split(" ");
+        for (String str : lists) {
+            if (!str.isBlank() && pat.matcher(str).matches()) {
+                count++;
+
+            }
+
+        }
+        return count;
+
+    }
+
 }
--- src/Leetcode/Start.java
+++ src/Leetcode/Start.java
@@ -15,23 +15,27 @@ public class Start {
      */
     public static void main(String[] args) {
    
-    //https://leetcode.com/problems/two-sum/    
-    var ts = new Two_Sum();   
-    int[] nums = {2,7,11,15};
-    int[] m = ts.twoSum(nums,9);
-    System.out.println(m[0] + " "+m[1]);    
+//    //https://leetcode.com/problems/two-sum/    
+//    var ts = new Two_Sum();   
+//    int[] nums = {2,7,11,15};
+//    int[] m = ts.twoSum(nums,9);
+//    System.out.println(m[0] + " "+m[1]);    
+//        
+//    //https://leetcode.com/problems/reverse-integer/    
+//    var ri = new ReverseInteger();
+//    int resultReverseInteger = ri.reverse(123);
+//    System.out.println(resultReverseInteger);        
+//     
+//    //https://leetcode.com/problems/palindrome-number/
+//    var pn = new PalindromeNumber();
+//    boolean resultPalindromeNumber = pn.isPalindrome(2332);
+//    System.out.println(resultPalindromeNumber);
+    
+   // https://leetcode.com/problems/number-of-valid-words-in-a-sentence/
+     var nv = new NumberOfWords();
+     int resultNumberOfWords = nv.countValidWords("he bought 2 pencils, 3 erasers, and 1  pencil-sharpener.");
+        System.out.println(resultNumberOfWords);   
         
-    //https://leetcode.com/problems/reverse-integer/    
-    var ri = new ReverseInteger();
-    int resultReverseInteger = ri.reverse(123);
-    System.out.println(resultReverseInteger);        
-     
-    //https://leetcode.com/problems/palindrome-number/
-    var pn = new PalindromeNumber();
-    boolean resultPalindromeNumber = pn.isPalindrome(2332);
-        System.out.println(resultPalindromeNumber);
-    
-    
     }
     
 }
